fa82252f0f58e35c9e52cd187ee56fb68b44c1e0
Александр Тумайкин <AATumaykin@tsum.ru>
2021-03-26 23:29:45 +0300

    fix: заменил функцию для чистки лишних символов

--- src/Domain/Electrolux/Helper/CleanHelper.php
+++ src/Domain/Electrolux/Helper/CleanHelper.php
@@ -8,6 +8,7 @@ class CleanHelper
 {
     public static function clean(string $message): string
     {
-        return trim($message, " \t\n\r\0\x0B\x01\x10\x03\x0f\x05\x04\x08");
+        return preg_replace('/[\x00-\x1F\x7F]/u', '', $message);
+//        return trim($message, " \t\n\r\0\x0B\x01\x10\x03\x0f\x05\x04\x08");
     }
 }
